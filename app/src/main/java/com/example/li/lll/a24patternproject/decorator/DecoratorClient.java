845338a8822e506cb8533c8f1ea45713a3bd44ec
lizhe998 <125956902@qq.com>
2018-11-07 00:06:38 +0800

    装饰链解释。

--- app/src/main/java/com/example/li/lll/a24patternproject/decorator/DecoratorClient.java
+++ app/src/main/java/com/example/li/lll/a24patternproject/decorator/DecoratorClient.java
@@ -2,6 +2,8 @@ package com.example.li.lll.a24patternproject.decorator;
 
 /**
  * 装饰模式
+ *      装饰模式客户端调用代码，装饰的过程更像是层层包装，用前面的对象装饰后面的对象。
+ * 装饰模式：动态地给一个对象添加一些额外的职责，就增加功能来说，装饰模式比生成子类更为灵活。
  */
 public class DecoratorClient {
 
