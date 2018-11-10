3b7a36168db2b1fc3f50fba59424f76e28ddfb04
lizhe998 <125956902@qq.com>
2018-11-10 22:56:24 +0800

    工厂方法模式

--- app/src/main/java/com/example/li/lll/a24patternproject/factory/method/FactoryClient.java
+++ app/src/main/java/com/example/li/lll/a24patternproject/factory/method/FactoryClient.java
@@ -2,6 +2,12 @@ package com.example.li.lll.a24patternproject.factory.method;
 
 import com.example.li.lll.a24patternproject.factory.sample.Operation;
 
+/**
+ *  使用简单工厂模式虽然去除了与具体产品的依赖（就是选择哪一个功能是不需要客户端来判断的，是由工厂模式判断的，但是当需要添加新的功能时候，不但对扩展开放了，对修改也开放了，违背了 开放-封闭的原则。
+ * 工厂方法模式：定义一个用于创建对象的接口，让子类决定实例化哪一个类。工厂方法使一个实例化延迟到其子类。
+ *
+ * 工厂方法模式实现时，客户端需要决定实例化哪一个工厂来实现运算类，选择判断的问题还是存在的，也就是说，工厂方法把简单工厂的内部逻辑判断移到了客户端代来进行，你想要的功能，本来是改工厂类的，二现在是修改客户端。
+ */
 public class FactoryClient {
     public static void main(String[] args){
         IFactory operFactory =new AddFactory();
--- app/src/main/java/com/example/li/lll/a24patternproject/factory/sample/Calculator.java
+++ app/src/main/java/com/example/li/lll/a24patternproject/factory/sample/Calculator.java
@@ -1,5 +1,9 @@
 package com.example.li.lll.a24patternproject.factory.sample;
 
+/**
+ * 简单工厂模式好处：在于工厂类包含必要的的逻辑判断，根据客户端的选择条动态实例化相关的类，对于客户端来说，去除了与具体产品的依赖。
+ *
+ */
 public class Calculator {
 
     public static void main(String [] args){
