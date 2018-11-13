b225d109cc3829c59d4b8f04a090c43b48c9572f
lizhe998 <125956902@qq.com>
2018-11-13 23:59:19 +0800

    观察者模式说明

--- app/src/main/java/com/example/li/lll/a24patternproject/observer/ObserverClient.java
+++ app/src/main/java/com/example/li/lll/a24patternproject/observer/ObserverClient.java
@@ -1,5 +1,15 @@
 package com.example.li.lll.a24patternproject.observer;
 
+/**
+ * 大话 P131
+ * 观察者模式：
+ * 定义： 定义了一种一对多的依赖关系，让多个观察者对象同时监听某一个主题对象。这个这题对象在状态发生变化时，
+ * 会通知所有观察者对象，使他们能够自己更新自己。
+ *
+ * 1.特点 讲一个系统分割成一系列相互协作的类有一个很不好的副作用，
+ * 那就是需要维护相关对象间的一致性，我们不许忘为了维护一致性而使得各类紧密耦合，这样会给维护扩展和重用带来不便。
+ *
+ */
 public class ObserverClient {
 
     public static void main(String[] args){
