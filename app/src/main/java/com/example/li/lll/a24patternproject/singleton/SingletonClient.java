1aa08ce6eedb093cfb0c0b28c6e11a4abc79053b
lizhe998 <125956902@qq.com>
2018-11-16 22:54:06 +0800

    任务链

--- app/src/main/java/com/example/li/lll/a24patternproject/singleton/SingletonClient.java
+++ app/src/main/java/com/example/li/lll/a24patternproject/singleton/SingletonClient.java
@@ -5,6 +5,9 @@ package com.example.li.lll.a24patternproject.singleton;
  *
  * 通常可以让一个全局变量使得一个对象被访问，但它不能防止你实例化多个对象多个对象，一个最好的办法就是，让类自身负责保存它的唯一实例。
  * 这个类可以保证没有其他实例可以被创建，并且它可以提供一个访问该实例的方法。
+ *
+ *
+ *  适用于：
  */
 public class SingletonClient {
     public static void main(String [] args){
