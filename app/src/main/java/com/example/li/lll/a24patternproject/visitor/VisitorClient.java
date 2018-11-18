e93f709b64830ca2a5b71a8977babe754ac8c0a0
lizhe998 <125956902@qq.com>
2018-11-18 23:01:30 +0800

    任务链

--- app/src/main/java/com/example/li/lll/a24patternproject/visitor/VisitorClient.java
+++ app/src/main/java/com/example/li/lll/a24patternproject/visitor/VisitorClient.java
@@ -1,5 +1,10 @@
 package com.example.li.lll.a24patternproject.visitor;
 
+/**
+ * 访问者模式： 表示用一个作用于某对象结构中的各元素的操作。它使你可以咋i不改变各个元素的类的前提下定义作用于这些原色的新操作。
+ *
+ * 访问者模式适合用于数据结构相对稳定的系统。
+ */
 public class VisitorClient {
     public static void main(String[] args){
         ObjectStructure o =new ObjectStructure();
