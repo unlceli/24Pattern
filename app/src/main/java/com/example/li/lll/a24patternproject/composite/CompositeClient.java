51893b69b882d03e776ad6d9587976fde670d8dc
lizhe998 <125956902@qq.com>
2018-11-21 22:21:52 +0800

    组合模式

--- app/src/main/java/com/example/li/lll/a24patternproject/composite/CompositeClient.java
+++ app/src/main/java/com/example/li/lll/a24patternproject/composite/CompositeClient.java
@@ -1,5 +1,11 @@
 package com.example.li.lll.a24patternproject.composite;
 
+/**
+ * 组合模式：将对象组合成树形结构以表示， ‘部分-整体’ 的层次结构，组合模式使得用户对单个对象和组合对象的使用具体一致性。
+ *
+ * 使用：当发现需求中体现部分与整体层次的机构时候，以及你希望用户可以忽略组合对象与单个对象的不同，统一地使用组合机构
+ * 中的所有对象时候，就应该考虑用组合模式。
+ */
 public class CompositeClient
     {
         public static void main(String[] args){
