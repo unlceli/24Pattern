111c223ac8dc844f34d03fa37e3e8486f6369e79
lizhe998 <125956902@qq.com>
2018-11-29 00:12:54 +0800

    策略和工厂模式、

--- app/src/main/java/com/example/li/lll/a24patternproject/strategyandfactory/CashContext.java
+++ app/src/main/java/com/example/li/lll/a24patternproject/strategyandfactory/CashContext.java
@@ -2,12 +2,28 @@ package com.example.li.lll.a24patternproject.strategyandfactory;
 
 public class CashContext {
 
-    CashSuper cs =null;
+    CashSuper cs = null;
 
-    public CashContext(String typr){
-        switch (typr){
+    public CashContext(String typr) {
+        switch (typr) {
             case "正常收费":
+                CashNormal cs0 = new CashNormal();
+                cs = cs0;
+                break;
 
+            case "满300返100":
+                CashReturn cs1 = new CashReturn("300", "100");
+                cs = cs1;
+                break;
+
+            case "打8折":
+                CashRebate cs2 = new CashRebate("0.8");
+                cs = cs2;
+                break;
         }
+    }
+
+    public double getResult(double money){
+        return  cs.acceptCash(money);
     }
 }
--- app/src/main/java/com/example/li/lll/a24patternproject/strategyandfactory/CashSuper.java
+++ app/src/main/java/com/example/li/lll/a24patternproject/strategyandfactory/CashSuper.java
@@ -4,6 +4,9 @@ public abstract class CashSuper {
     public abstract double acceptCash(double money);
 }
 
+/**
+ * 正常
+ */
 class CashNormal extends CashSuper{
 
     @Override
@@ -12,15 +15,48 @@ class CashNormal extends CashSuper{
     }
 }
 
+/**
+ * 打折
+ */
 class CashRebate extends CashSuper{
 
     private double moneyRebate =1d;
 
 
+    public CashRebate(String moneyRebate){
+        this.moneyRebate = Double.parseDouble(moneyRebate);
+
+    }
+
 
 
     @Override
     public double acceptCash(double money) {
-        return 0;
+        return money * moneyRebate;
+    }
+}
+
+/**
+ * 返利   300 反 100；
+ */
+class CashReturn extends CashSuper{
+
+    private double moneyCondition = 0.0d;
+    private double moneyReturn = 0.0d;
+
+    public CashReturn(String moneyCondition,String moneyReturn){
+        this.moneyCondition =Double.parseDouble(moneyCondition);
+        this.moneyReturn =Double.parseDouble(moneyReturn);
+    }
+
+
+
+    @Override
+    public double acceptCash(double money) {
+        double result =money;
+        if (money >= moneyCondition){
+            result  =money -Math.floor(money/moneyCondition)*moneyReturn;
+        }
+        return result ;
     }
 }
